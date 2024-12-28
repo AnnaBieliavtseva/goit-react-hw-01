1b1cd192e396eb9d347d0974dd00ad6d8ebb98d6
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2024-12-28 12:53:13 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Profile from './components/Profile'
+
+function App() {
+
+  return (
+    <div>
+      <div><Profile /></div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import css from './Profile.module.css';
+
+console.log(css);
+
+
+ function Profile () {
+  
+  return <h1 className={css.title}>My project</h1>;
+};
+
+export default Profile;
