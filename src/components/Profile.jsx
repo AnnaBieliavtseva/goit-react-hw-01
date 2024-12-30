e1e7279fd1096080b42a471600d41906844d9586
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2024-12-30 14:40:43 +0100

    finish 1 tssk - Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,9 +3,35 @@ import css from './Profile.module.css';
 console.log(css);
 
 
- function Profile () {
+ export default function Profile ({name:username, tag, location, image:avatar, stats: {followers, views, likes}}) {
   
-  return <h1 className={css.title}>My project</h1>;
+   return <div className={css.profile_container}>
+  <div className={css.profile_img_thumb}>
+    <img className={css.profile_img}
+      src={avatar}
+        alt={username}
+        width={80}
+        height={80}
+    />
+    <p className={css.profile_name}>{username}</p>
+       <p className={css.profile_top_text}>@{tag}</p>
+    <p className={css.profile_top_text}>{location}</p>
+  </div>
+
+  <ul className={css.profile_list}>
+    <li className={css.profile_list_item}>
+      <span>Followers</span>
+      <span className={css.profile_list_text}>{followers}</span>
+    </li>
+    <li className={css.profile_list_item}>
+      <span>Views</span>
+      <span className={css.profile_list_text}>{views}</span>
+    </li>
+    <li className={css.profile_list_item}>
+      <span>Likes</span>
+      <span className={css.profile_list_text}>{likes}</span>
+    </li>
+  </ul>
+</div>
 };
 
-export default Profile;
